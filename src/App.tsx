56ac91445003107e08518c425771842e02bd7cd7
sharon0511 <sharon05110511@gmail.com>
2023-11-09 17:42:21 +0900

    Typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="lightgreen" borderColor="yellow" />
+      <Circle bgColor="pink" text="I'm Circle" />
+    </div>
+  );
+}
+
+export default App;
