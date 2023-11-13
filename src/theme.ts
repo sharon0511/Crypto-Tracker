5c26536896c16b30722a53c82e47233550acacc7
sharon0511 <sharon05110511@gmail.com>
2023-11-13 18:24:14 +0900

    Route State

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "black",
-  btnColor: "lightgreen",
-};
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "whitesmoke",
-  btnColor: "pink",
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#8c7ae6",
 };
